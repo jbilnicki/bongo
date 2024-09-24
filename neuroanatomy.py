f33aa7788370925cf26d927881a1a949fc465b07
jbilnicki <182365176+jbilnicki@users.noreply.github.com>
2024-09-24 13:41:11 +0200

    Added check_VTA function and updated chack_NAc

--- neuroanatomy.py
+++ neuroanatomy.py
@@ -86,23 +86,67 @@ def manual_superimpose(img1_path, img2_path, direction='right'):
     cv2.destroyAllWindows()
 
 
-def check_NAc(tissue_image, ap_cor=1.2, hemisphere='right'):
+def check_structure(tissue_image, max_anterior, max_posterior, ap_cor, hemisphere='right'):
     
+    ''' Function to add mask from stereotactic atlas to brain tissue image
+        to check in which subregion is the ROI
+        
+        tissue_image (string) path to our image
+        ap_cor (float) anterior-posterior disatnce in mm from bregma to our ROI
+        specifies which mask program should use
+        hemisphere (string) 'right' or 'left' default = 'right' mask images are 
+        from right orientation so if image is from left hemisphere program flips mask
+    '''
+    
+    
+    try:
+        float(ap_cor)
+        mask_img = f'img/slice_{ap_cor}.png'
+    except:
+        raise TypeError("Inapropriate ap_core inupt: should be float")
+        
+    
+    if ap_cor < 2.0 and ap_cor > 0.9:
+        manual_superimpose(tissue_image, mask_img, hemisphere)
+        
+    else:
+        raise ValueError("Sorry provided coordinate is not available")
+        
+    
+    
+def check_NAc(tissue_image, ap_cor=1.2, hemisphere='right'):
     ''' Function to add mask from stereotactic atlas to tissue image from
         nucleus accumbens to check whether roi is in core or shell of this nucleus
         
         tissue_image (string) path to our image
-        ap_cor (float) anterior-posterior disatnce in mm from bregma to our roi
+        ap_cor (float) anterior-posterior disatnce in mm from bregma to our ROI
         default = 1.2 specifies which mask program should use
         hemisphere (string) 'right' or 'left' default = 'right' mask images are 
         from right orientation so if image is from left hemisphere program flips mask
     '''
     
-    try:
-        float(ap_cor)
-        mask_img = f'img/NAc_{ap_cor}.png'
-    except:
-        raise ValueError("Inapropriate ap_core inupt: should be float")
-        
+    # range of available coordinates from sterotactic atlas 
+    max_anterior = 2.0
+    max_posterior = 0.9
     
-    manual_superimpose(tissue_image, mask_img, hemisphere)
+    check_structure(tissue_image, max_anterior, max_posterior, ap_cor, hemisphere)
+    
+    
+    
+def check_VTA(tissue_image, ap_cor=5.2, hemisphere='right'):
+    ''' Function to add mask from stereotactic atlas to tissue image 
+        and check if our ROI is in ventral tegmental area or in substantia nigra
+        
+        tissue_image (string) path to our image
+        ap_cor (float) anterior-posterior disatnce in mm from bregma to our ROI
+        default = 5.2 specifies which mask program should use
+        hemisphere (string) 'right' or 'left' default = 'right' mask images are 
+        from right orientation so if image is from left hemisphere program flips mask
+    '''
+    
+    # range of available coordinates from sterotactic atlas 
+    max_anterior = 4.8
+    max_posterior = 6.0
+    
+    check_structure(tissue_image, max_anterior, max_posterior, ap_cor, hemisphere)
+    
