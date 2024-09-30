a9ea870069723b2a691293e61edde35d11ef2d06
jbilnicki <182365176+jbilnicki@users.noreply.github.com>
2024-09-30 21:45:15 +0200

    moving setup.py to main directory

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0'
+DESCRIPTION = "Bongo Offers Neuroscientific Graphs and Outputs"
+
+setup(
+        name='bongo',
+        version=VERSION,
+        author='Jakub Bilnicki',
+        author_email="",
+        url='https://github.com/jbilnicki/bongo.git',
+        description=DESCRIPTION,
+        packages=find_packages(),
+        install_requires=[
+        'numpy', 'pandas', 'matplotlib', 'seaborn', 'opencv-python','scipy'
+        ],
+        
+        keywords=['python', 'voltammertry', 'neuroanatomy'],
+        classifiers=[
+        "Intended Audience :: Researchers",
+        "Intended Audience :: Students",
+        "Programming Language :: Python :: 3",
+        "License :: GPU License",
+        "Operating System :: OS Independent"
+        ]
+        
+     )
