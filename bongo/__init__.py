a42582a761e1bdf8f283617c019cc1e7306b36c6
jbilnicki <182365176+jbilnicki@users.noreply.github.com>
2024-09-30 17:16:55 +0200

    making bongo a real package

---
+++ bongo/__init__.py
@@ -0,0 +1,13 @@
+"""
+
+bongo
+
+bongo-"Bongo Offers Neuroscientific Graphs and Outputs"
+is a python module for analysis of basic neuroscientific data.
+
+"""
+
+
+__version__ = "1.0"
+__author__ = "Jakub Bilnicki"
+
