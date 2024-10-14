fbe1d7dc05cdeb43216ff6deddb2961777bd76ab
jbilnicki <182365176+jbilnicki@users.noreply.github.com>
2024-10-14 19:25:44 +0200

    change __init__.py

--- bongo/__init__.py
+++ bongo/__init__.py
@@ -11,3 +11,7 @@ is a python module for analysis of basic neuroscientific data.
 __version__ = "1.0"
 __author__ = "Jakub Bilnicki"
 
+from . import behaviour
+from . import cells
+from . import neuroanatomy
+from . import voltammetry
