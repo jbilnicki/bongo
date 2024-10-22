31d1a64ce64af8ef27b521943adcec7b30103d16
jbilnicki <182365176+jbilnicki@users.noreply.github.com>
2024-10-22 15:19:56 +0200

    add photometry.py
    this new file is for analyzing some data obtained with fiber photometry

---
+++ bongo/photometry.py
@@ -0,0 +1,13 @@
+from scipy.signal import find_peaks
+from scipy.integrate import simpson
+from scipy.stats import mannwhitneyu
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# function for calculating background before CS
+
+# find maximal peak
+
+# area under the curve
+
