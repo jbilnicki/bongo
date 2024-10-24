7454c0b008684ee3c8077b3258b43916a7e455b2
jbilnicki <182365176+jbilnicki@users.noreply.github.com>
2024-10-24 13:18:57 +0200

    modify photometry.py and example.py
    fixed function for area under curve calculating

--- bongo/example.py
+++ bongo/example.py
@@ -7,22 +7,38 @@ Created on Sat Sep 21 10:07:02 2024
 
 
 
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #import voltammetry
 
-
+import numpy as np
 import photometry
 
 file = photometry.open_file("~/Desktop/example.csv")
 
-cs_1 = file["DeltaF/F-1"]
-
-peak = photometry.normalize_peak(cs_1)
-
-print(f'Normalized value of your peak is: {peak}')
-
-auc = photometry.normalize_auc(cs_1)
-print(auc)
+mean = []
+for i in range(1,10):
+    
+    cs_1 = file[f"DeltaF/F-{i}"]
+    
+    print("-------------------------------------------------")
+    print(f"DeltaF/F-{i}")
+    
+    plt.figure()
+    plt.title(f"DeltaF/F-{i}")
+    peak = photometry.normalize_peak(cs_1)
+    
+    print(f'Normalized value of your peak is: {peak}')
+    
+    plt.figure()
+    plt.title(f"DeltaF/F-{i}")
+    auc, normalized_auc = photometry.normalize_auc(cs_1)
+    print(f'row auc: {auc}')
+    print(f'normalized auc: {normalized_auc}')
+    mean.append(peak)
+    
+mean = np.array(mean)
+m = np.mean(mean)
+print(f'mean: {m}')
 
 
 '''
--- bongo/photometry.py
+++ bongo/photometry.py
@@ -25,18 +25,14 @@ def open_file(path, n_cs=10):
     
 
 
-
-def normalize_peak(array, base_length=5, row_duration=50):
-    
-    ''' function for assesing percentage of change in fluorescence during some event
+def set_baseline(array, base_length=5, row_duration=50):
+    ''' function for dividing array into baseline and values after treatment/event
     
     array - array-like object with data from specigic event
     base_length - float number of seconds prioror to our event that will be counted as a baseline
     row_duration (int default=50) what is difference in time from one row to the other (in miliseconds)
     
-    then function plots results
-    
-    return normalised_peak - value of fluorescence in a maximal peak normalised to the baseline
+    return base - array with values for baseline, base_end - index of the last baseline value
     '''
     
     try: 
@@ -56,7 +52,34 @@ def normalize_peak(array, base_length=5, row_duration=50):
     base_end = int(base_end)
     base = array[1:base_end]
     
-    event = array[base_end:]
+    return base, base_end
+    
+    
+def normalize_peak(array, base_length=5, row_duration=50, event_length=6):
+    
+    ''' function for assesing percentage of change in fluorescence during some event
+    
+    array - array-like object with data from specigic event
+    base_length - float number of seconds prioror to our event that will be counted as a baseline
+    row_duration (int default=50) what is difference in time from one row to the other (in miliseconds)
+    event_length (float default=5) number of seconds after the baseline
+    for which maximal peak will be found
+    
+    then function plots results
+    
+    return normalised_peak - value of fluorescence in a maximal peak normalised to the baseline
+    '''
+    
+    base, base_end = set_baseline(array, base_length, row_duration)
+    
+    # event for area under curve starts after baseline and lasts for a givent period
+    event_duration = event_length/row_duration*1000
+    event_duration = int(event_duration)
+    
+    # index of the last value counted as event
+    event_end = base_end + event_duration
+    
+    event = array[base_end:event_end]
     
     # find peaks during our event
     peaks,_ = find_peaks(event)
@@ -66,7 +89,7 @@ def normalize_peak(array, base_length=5, row_duration=50):
     max_peak = max(event)
     max_peak_index = np.where(event==max_peak)
     
-    normalised_peak = max_peak/np.mean(base)*100
+    normalised_peak = max_peak/np.mean(base)
     
     # correction for negative baseline
     if np.mean(base) < 0:
@@ -80,29 +103,65 @@ def normalize_peak(array, base_length=5, row_duration=50):
     event_x = np.array(event_x)
     event_x += base_end
     
+    plt.plot(array, color='orange')
     plt.plot(base, color='blue')
-    plt.plot(event_x, event, color='orange')
-    plt.scatter(max_peak_index[0][0]+base_end, max_peak, color='red')
+    plt.plot(event_x, event, color='red')
+    plt.scatter(max_peak_index[0][0]+base_end, max_peak, color='green')
     
     return normalised_peak
     
 
-def normalize_auc(array):
+def normalize_auc(array, base_length=5, row_duration=50, event_length=5):
     
     ''' function calculates area under curve for peak in fluorescence and normalize it to the background
-    array - array-like object with fluorescence data'''
     
-    try: 
-        array = np.array(array)
-    except:
-        raise TypeError("Can't convert your data to numpy.array")
+    array - array-like object with fluorescence data
+    base_length - float number of seconds prioror to our event that will be counted as a baseline
+    row_duration (int default=50) what is difference in time from one row to the other (in miliseconds)
+    event_length (float default=5) number of seconds after the baseline
+    for which area under the curve will be calculated
+    
+    then function plots results
+    
+    return event_auc - float value of area under curve for given by event_length period, 
+    standarized_auc - float value of area under curve for given by event_length period 
+    normalized with baseline mean value
+    '''
+    
+    base, base_end = set_baseline(array, base_length, row_duration)
+    
+    # event for area under curve starts after baseline and lasts for a givent period
+    event_duration = event_length/row_duration*1000
+    event_duration = int(event_duration)
+    
+    # index of the last value counted as event
+    event_end = base_end + event_duration
+    
+    event = array[base_end:event_end]
     
     # drop nan values for simpson algorithm to work properly
-    array = array[~np.isnan(array)]
+    event = event[~np.isnan(event)]
     
     
-    auc = simpson(array)
+    x = range(0,len(event))
+    x = np.array(x)
+    x += base_end
     
-    return auc
+    # area under the curve for event and for baseline
+    event_auc = simpson(event)
+    base_auc = simpson(base)
+    
+    # normalization
+    normalized_auc = event_auc/base_auc
+    
+    
+    # plot results
+    plt.plot(array, color='orange', label='other data')
+    plt.plot(base, color='blue', label='baseline')
+    plt.plot(x, event, color='red', label='event')
+    plt.legend()
+    
+    
+    return event_auc, normalized_auc
 
 
